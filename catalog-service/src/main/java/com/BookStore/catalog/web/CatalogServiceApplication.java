4e4361ae1d614f7a5180a288f53fafdab546c216
Madineni Jayasree <jayasreemadineni20@gmail.com>
2025-06-07 19:03:28 +0530

    "Changes in github configurations"

--- catalog-service/src/main/java/com/BookStore/catalog/web/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/BookStore/catalog/web/CatalogServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 // @EnableConfigurationProperties(ApplicationProperties.class)
 @ConfigurationPropertiesScan
+
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
